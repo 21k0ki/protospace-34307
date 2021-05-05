65ecc7eb0b829112d1e3458dc0c54a956a6950c4
21k0ki <gogo.21koki@gmail.com>
2021-05-05 16:46:14 +0900

    Update prototypes_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @prototypes = Prototype.all
@@ -31,9 +32,6 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    unless user_signed_in?
-      redirect_to action: index
-    end
   end
 
   def update
@@ -55,4 +53,11 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:image, :title, :cath_copy, :concept).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+   if user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 end
